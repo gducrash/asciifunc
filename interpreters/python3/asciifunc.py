0726bf4465374599b9ae95883def4fd3f6bcbda4
Dexter Hill <49513414+DexterHill0@users.noreply.github.com>
2021-06-24 20:50:53 +0100

    added strict mode

--- interpreters/python3/asciifunc.py
+++ interpreters/python3/asciifunc.py
@@ -1,20 +1,30 @@
 import argparse
 from pathlib import Path
 
-import pprint
-
 from tokenise import tokenise
+from interpreter import interpret
 
 parser = argparse.ArgumentParser(description="Runs an asciifunc file.")
 
 parser.add_argument("--file", "-f",
                     dest="file",
                     help="Path to the file.",
+                    required=True,
                     default=None)
+
+parser.add_argument("--strict", "-s",
+                    dest="strict",
+                    action="store_true",
+                    help="Strict mode (raises errors).",
+                    required=False,
+                    default=False)
 
 args = parser.parse_args()
 
 if(args.file is None or not Path(args.file).exists()):
     raise ValueError("File does not exist!")
 
-pprint.pprint(tokenise(args.file))
+STRICT = args.strict
+
+tok = tokenise(args.file)
+interpret(tok, STRICT)
